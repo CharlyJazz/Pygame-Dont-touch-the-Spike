ae527273f7eb04e8ba7a2a0fc60413cf1108353f
Aramís Segovia <aramissegovia@gmail.com>
2020-07-05 05:45:00 -0400

    fix: add black as a development package
    also format according to black
    
    fixes #3

--- Font.py
+++ Font.py
@@ -5,9 +5,9 @@ title1 = "Loco"
 title2 = "Parajo"
 title3 = "Mexicano"
 
-x_title1 = 26+10
-x_title2 = 32+35
-x_title3 = 46+50
+x_title1 = 26 + 10
+x_title2 = 32 + 35
+x_title3 = 46 + 50
 
 x_score = 220
 x_life = 3
@@ -15,8 +15,8 @@ Tusj = "FFF_Tusj.ttf"
 caveman = "CAVEMAN_.TTF"
 towerruins = "towerruins.ttf"
 copy = "Developed by Carlos Azuaje | 2016"
-caption = 'Loco Parajo Mexicano'
-try_again = ['Try Again ?', 'Press a Key for Play']
+caption = "Loco Parajo Mexicano"
+try_again = ["Try Again ?", "Press a Key for Play"]
 
 
 class Text(object):
@@ -31,24 +31,47 @@ class Text(object):
 
 class Create_life(Text):
     def __init__(self, life):
-        self.LifeText = Text(Tusj, 20, "Life : {}".format(str(life)), (0, 0, 0), x_life, -2)
+        self.LifeText = Text(
+            Tusj, 20, "Life : {}".format(str(life)), (0, 0, 0), x_life, -2
+        )
 
 
 class Create_Score(Text):
     def __init__(self, score):
-        self.ScoreText = Text(Tusj, 20, "Score : {}".format(str(score)), (0, 0, 0), x_score, -2)
-        self.ScoreEnd = Text(towerruins, 20, "S c o r e  {}".format(str(score)), (0, 0, 0), x_title3-40, 450)
+        self.ScoreText = Text(
+            Tusj, 20, "Score : {}".format(str(score)), (0, 0, 0), x_score, -2
+        )
+        self.ScoreEnd = Text(
+            towerruins,
+            20,
+            "S c o r e  {}".format(str(score)),
+            (0, 0, 0),
+            x_title3 - 40,
+            450,
+        )
 
 
 class Create_menu(Text):
     def __init__(self):
-        self.title1 = Text(caveman, 30, "{}".format(title1), (100, 160, 221), x_title1, 80)
-        self.title2 = Text(caveman, 30, "{}".format(title2), (150, 140, 200), x_title2, 110)
-        self.title3 = Text(caveman, 30, "{}".format(title3), (200, 120, 180), x_title3, 140)
-        self.copy_right = Text(towerruins, 25, "{}".format(copy), (119, 136, 153), -2, 570)
+        self.title1 = Text(
+            caveman, 30, "{}".format(title1), (100, 160, 221), x_title1, 80
+        )
+        self.title2 = Text(
+            caveman, 30, "{}".format(title2), (150, 140, 200), x_title2, 110
+        )
+        self.title3 = Text(
+            caveman, 30, "{}".format(title3), (200, 120, 180), x_title3, 140
+        )
+        self.copy_right = Text(
+            towerruins, 25, "{}".format(copy), (119, 136, 153), -2, 570
+        )
 
 
 class Create_TryAgain(Text):
     def __init__(self):
-        self.try_again = Text(towerruins, 30, "{}".format(try_again[0]), (153, 0, 0), x_title1+5, 350)
-        self.press_key = Text(towerruins, 20, "{}".format(try_again[1]), (204, 0, 0), x_title2-60, 400)
+        self.try_again = Text(
+            towerruins, 30, "{}".format(try_again[0]), (153, 0, 0), x_title1 + 5, 350
+        )
+        self.press_key = Text(
+            towerruins, 20, "{}".format(try_again[1]), (204, 0, 0), x_title2 - 60, 400
+        )
--- Variables.py
+++ Variables.py
@@ -3,11 +3,11 @@ import pygame
 
 side1 = 50
 side2 = 25
-HEIGHT = 500+2*side1
-WIDTH = 300+2*side2
-caption = 'Dont touch the spikes'
+HEIGHT = 500 + 2 * side1
+WIDTH = 300 + 2 * side2
+caption = "Dont touch the spikes"
 size = [WIDTH, HEIGHT]
-files = ['b.png', 'd.png', 'o.png', 'p.png', 'jump.mp3', 'music.mp3']
+files = ["b.png", "d.png", "o.png", "p.png", "jump.mp3", "music.mp3"]
 background_game, spike, gameover, bird, jump, music = files
 screen = pygame.display.set_mode(size)
 game_on = False
